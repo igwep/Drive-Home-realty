aed7addf7570d31d80e63d048d121015402b834e
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-07-19 13:02:41 +0100

    commiting

--- js/app.js
+++ js/app.js
@@ -14,3 +14,24 @@ burger.addEventListener('click', ()=>{
        
     
 })
+//carosel
+
+
+document.addEventListener('DOMContentLoaded', ()=>{
+    const carosel = document.querySelector('.carosel');
+    let scrollAmount = 0;
+    const itemWidth = document.querySelector('.carousel-item').offsetWidth;
+    
+    setInterval(() => {
+      scrollAmount += itemWidth;
+      if (scrollAmount >= carosel.scrollWidth - carosel.clientWidth) {
+        scrollAmount = 0; // Reset to the beginning when reaching the end
+      }
+      carosel.scrollTo({
+        top: 0,
+        left: scrollAmount,
+        behavior: 'smooth' // Smooth scrolling
+      });
+    }, 3000); // Change image ev
+
+})
