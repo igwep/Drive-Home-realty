c71f086d31ee6a355aa339f7f261dcb27f23212a
IGWE PRECIOUS IFECHUKWU <igwep5537@gmail.com>
2024-07-20 14:30:08 +0100

    added about

--- js/app.js
+++ js/app.js
@@ -1,4 +1,23 @@
-const burger = document.querySelector('.burger-menu');
+/* const burger = document.querySelector('.burger-menu');
+console.log('burger')
+const mobileContainer = document.querySelector('.mobile-link-container');
+burger.addEventListener('click', ()=>{
+    console.log('why')
+    if(mobileContainer.classList.contains('h-0')){
+        mobileContainer.classList.remove('h-0');
+        //mobileContainer.classList.remove('hidden');
+        mobileContainer.classList.add('h-48');
+    }
+    else{
+        mobileContainer.classList.remove('h-48');
+        //mobileContainer.classList.add('hidden');
+        mobileContainer.classList.add('h-0');
+    }
+       
+    
+}) */
+ export const burgerMenu = ()=>{
+  const burger = document.querySelector('.burger-menu');
 console.log('burger')
 const mobileContainer = document.querySelector('.mobile-link-container');
 burger.addEventListener('click', ()=>{
@@ -16,6 +35,8 @@ burger.addEventListener('click', ()=>{
        
     
 })
+}
+burgerMenu();
 //carosel
 
 
@@ -37,17 +58,12 @@ document.addEventListener('DOMContentLoaded', ()=>{
     }, 3000); // Change image ev
 
 })
-  // Initialize intl-tel-input
-  $(document).ready(function() {
-    $("#mobile_code").intlTelInput({
-      initialCountry: "in", // Default country
-      separateDialCode: true, // Show separate dial code
-      utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.19/js/utils.js" // Optional: for formatting and validation
+    const phoneInputField = document.querySelector("#phone");
+    const phoneInput = window.intlTelInput(phoneInputField, {
+      utilsScript:
+        "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
     });
-  });
-  import intlTelInput from 'intl-tel-input';
-
-const input = document.querySelector(".phone");
-intlTelInput(input, {
-    utilsScript: "path/to/utils.js"
-});
+    const btn = document.querySelector('.btn');
+    btn.addEventListener('click', ()=>{
+        console.log(phoneInput);
+    })
